6ba4c0c5fef9357be32abeda7511b2e714ea90d7
pedro-drosa <pedro_drosa@hotmail.com>
2021-03-08 09:50:15 -0300

    added: uniform/uniform on the project

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi';
+import { Form } from '@unform/web';
 
 import { Container, Content, Background } from './styles';
 
@@ -9,12 +10,16 @@ import Input from '../../components/Input';
 import Button from '../../components/Button';
 
 const SignUp: React.FC = () => {
+  function handleSubmit(data: any): void {
+    console.log(data);
+  }
+
   return (
     <Container>
       <Background />
       <Content>
         <img src={logo} alt="Gobarber" />
-        <form>
+        <Form onSubmit={handleSubmit}>
           <h1>Faça seu Cadastro</h1>
           <Input name="user" icon={FiUser} type="text" placeholder="Nome" />
           <Input name="email" icon={FiMail} type="text" placeholder="E-mail" />
@@ -25,7 +30,7 @@ const SignUp: React.FC = () => {
             placeholder="Senha"
           />
           <Button type="submit">Cadastrar</Button>
-        </form>
+        </Form>
         <a href="createaccount">
           <FiArrowLeft />
           Voltar para Login
