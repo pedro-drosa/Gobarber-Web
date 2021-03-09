fa4dbbbfb57179b580e03ab95493383d9a83c7c8
pedro-drosa <pedro_drosa@hotmail.com>
2021-03-09 19:29:54 -0300

    validation with YUP

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi';
+import React, { useCallback } from 'react';
 import { Form } from '@unform/web';
+import * as Yup from 'yup';
+import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi';
 
 import { Container, Content, Background } from './styles';
 
@@ -10,9 +11,26 @@ import Input from '../../components/Input';
 import Button from '../../components/Button';
 
 const SignUp: React.FC = () => {
-  function handleSubmit(data: any): void {
-    console.log(data);
-  }
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  const handleSubmit = useCallback(async (data: object) => {
+    try {
+      const schemma = Yup.object().shape({
+        name: Yup.string().required('Nome é obrigatório'),
+        email: Yup.string()
+          .required('E-mail é obrigatório')
+          .email('Digite um email válido'),
+        password: Yup.string().min(
+          6,
+          'A senha deve conter no mínimo 6 digitos',
+        ),
+      });
+      await schemma.validate(data, {
+        abortEarly: false,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }, []);
 
   return (
     <Container>
@@ -21,7 +39,7 @@ const SignUp: React.FC = () => {
         <img src={logo} alt="Gobarber" />
         <Form onSubmit={handleSubmit}>
           <h1>Faça seu Cadastro</h1>
-          <Input name="user" icon={FiUser} type="text" placeholder="Nome" />
+          <Input name="name" icon={FiUser} type="text" placeholder="Nome" />
           <Input name="email" icon={FiMail} type="text" placeholder="E-mail" />
           <Input
             name="password"
