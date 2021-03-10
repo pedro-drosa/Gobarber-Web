115334bdc7b30d331be4efe7522f009c3b2b0217
pedro-drosa <pedro_drosa@hotmail.com>
2021-03-10 08:54:11 -0300

    created: login validation

--- src/pages/Signin/index.tsx
+++ src/pages/Signin/index.tsx
@@ -1,19 +1,44 @@
-import React from 'react';
+import React, { useCallback, useRef } from 'react';
+import { Form } from '@unform/web';
+import { FormHandles } from '@unform/core';
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
+import * as Yup from 'yup';
 
-import { Container, Content, Background } from './styles';
+import getValidationErrors from '../../utils/getValidationErrors';
 
 import logo from '../../assets/logo.svg';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
+import { Container, Content, Background } from './styles';
+
 const SignIn: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  const handleSubmit = useCallback(async (data: object) => {
+    try {
+      formRef.current?.setErrors({});
+      const schemma = Yup.object().shape({
+        email: Yup.string()
+          .required('E-mail é obrigatório')
+          .email('Digite um email válido'),
+        password: Yup.string().required('Senha obrigatória'),
+      });
+      await schemma.validate(data, {
+        abortEarly: false,
+      });
+    } catch (err) {
+      const errors = getValidationErrors(err);
+      formRef.current?.setErrors(errors);
+    }
+  }, []);
+
   return (
     <Container>
       <Content>
         <img src={logo} alt="Gobarber" />
-        <form>
+        <Form ref={formRef} onSubmit={handleSubmit}>
           <h1>Faça seu Login</h1>
           <Input name="email" icon={FiMail} type="text" placeholder="E-mail" />
           <Input
@@ -24,7 +49,7 @@ const SignIn: React.FC = () => {
           />
           <Button type="submit">Entrar</Button>
           <a href="forgot">Esqueci minha senha</a>
-        </form>
+        </Form>
         <a href="createaccount">
           <FiLogIn />
           Criar Conta
