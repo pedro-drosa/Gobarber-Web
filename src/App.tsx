8b296f948b207ceb7079e9a9b9a0266cbfd68b89
pedro-drosa <pedro_drosa@hotmail.com>
2021-03-13 15:30:36 -0300

    created:Toast with styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import GlobalStyle from './styles/global';
 
 import SignIn from './pages/SignIn';
-
+import ToastContainer from './components/ToastContainer';
 import { AuthProvider } from './hooks/AuthContext';
 
 const App: React.FC = () => {
@@ -11,6 +11,7 @@ const App: React.FC = () => {
       <AuthProvider>
         <SignIn />
       </AuthProvider>
+      <ToastContainer />
       <GlobalStyle />
     </>
   );
